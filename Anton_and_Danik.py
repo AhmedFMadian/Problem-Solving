0f6984c84a5cca2bf298ba97f56554ddeeaee9f5
AhmedFMadian <amadian2005@gmail.com>
2025-08-12 10:21:40 +0300

    Update Anton_and_Danik.py

--- Anton_and_Danik.py
+++ Anton_and_Danik.py
@@ -1,15 +1,11 @@
 n=input()
 s=list(input())
-A=0
-D=0
-for i in range(len(s)):
-    if s[i]=="A":
-        A+=1
-    else:
-        D+=1
+A=s.count("A")
+D=s.count("D")
 if A>D:
     print("Anton")
 elif D>A:
     print("Danik")
 else:
     print("Friendship")
+
