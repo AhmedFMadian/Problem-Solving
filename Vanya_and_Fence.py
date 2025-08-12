515e8155c3ba1e56b5d4723830178e1d560733fa
AhmedFMadian <amadian2005@gmail.com>
2025-08-12 09:41:47 +0300

    Add files via upload

---
+++ Vanya_and_Fence.py
@@ -0,0 +1,10 @@
+n,h = map(int, input().split())
+a=list(map(int, input().split()))
+width:int=0
+for i in range(n):
+    if a[i]>h:
+        width += 2
+    else:
+        width += 1
+
+print(width)
